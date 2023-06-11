e875979a7981d10ff2e36e6288d076469220584c
Philip Nduka <philip.igboba@mkobo.com.ng>
2023-06-11 08:20:47 +0100

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by APPLE on 6/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
