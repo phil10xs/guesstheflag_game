3bd13984b6e43a21da16c7d053499107d1708bb8
Philip Nduka <philip.igboba@mkobo.com.ng>
2023-06-15 05:02:34 +0100

    guess the flag- features updated

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -2,65 +2,102 @@
 //  ViewController.swift
 //  Project2
 //
-//  Created by APPLE on 6/11/23.
+//  Created by TwoStraws on 13/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet var button1: UIButton!
+	@IBOutlet var button2: UIButton!
+	@IBOutlet var button3: UIButton!
 
-    @IBOutlet var button1: UIButton!
-    @IBOutlet var button2: UIButton!
-    @IBOutlet var button3: UIButton!
-    
-    var countries = [String]()
-    var score = 0
-    var correctAnswer = 0
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        button1.layer.borderWidth = 1
-        button2.layer.borderWidth = 1
-        button3.layer.borderWidth = 1
+	var countries = [String]()
+	var correctAnswer = 0
+	var score = 0
+    var noOfAnswersReceived = 0
+    var rightAnswer = ""
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		button1.layer.borderWidth = 1
+		button2.layer.borderWidth = 1
+		button3.layer.borderWidth = 1
+
+		button1.layer.borderColor = UIColor.lightGray.cgColor
+		button2.layer.borderColor = UIColor.lightGray.cgColor
+		button3.layer.borderColor = UIColor.lightGray.cgColor
+
+		countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
-        button1.layer.borderColor = UIColor.lightGray.cgColor
-        button2.layer.borderColor = UIColor.lightGray.cgColor
-        button3.layer.borderColor = UIColor.lightGray.cgColor
-        countries += ["estonia","france","germany","ireland", "italy", "monaco", "nigeria", "poland","russia", "spain", "us","uk"  ]
-        
-        askQuestion()
-      
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    func askQuestion(action: UIAlertAction! = nil){
-        countries.shuffle()
-        button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button2.setImage(UIImage(named: countries[1]), for: .normal)
-        button3.setImage(UIImage(named: countries[2]), for: .normal)
+		askQuestion()
+	}
+
+	func askQuestion(action: UIAlertAction! = nil) {
+		countries.shuffle()
+
+		button1.setImage(UIImage(named: countries[0]), for: .normal)
+		button2.setImage(UIImage(named: countries[1]), for: .normal)
+		button3.setImage(UIImage(named: countries[2]), for: .normal)
+
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+		title = "\(countries[correctAnswer].uppercased())  - Your score is \(score)"
+	}
+    
+    func  isOver() -> Bool
+    {
+        return noOfAnswersReceived == 10
+    }
+    
+    
+    func trackQuestionAndEndGame(){
+        noOfAnswersReceived += 1
+        if (isOver()){
+            title = "The Game is Over."
+            noOfAnswersReceived = 0
+            score = 0
+        }
     }
 
-
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        var title: String
-
+	@IBAction func buttonTapped(_ sender: UIButton) {
+		var title = ""
+        
+        func  isAnswerWrong() -> Bool
+        {
+            return title == "Wrong"
+        }
+     
+     
         if sender.tag == correctAnswer {
-            title = "Correct"
-            score += 1
+                title = "Correct"
+                score += 1
+            rightAnswer = ""
         } else {
-            title = "Wrong"
-            score -= 1
+                title = "Oops!! Wrong"
+            rightAnswer = "The flag belong to \(countries[sender.tag].uppercased()) "
+                score -= 1
         }
         
-        
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
-    }
-}
+    
+            
+        if (noOfAnswersReceived == 9){
+            title = "Horray!!"
+            let ac = UIAlertController(title: title, message: "Your final score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Start All Over", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        } else {
+           
+           
+            
+            let ac = UIAlertController(title: title, message: "Your score is \(score). \(rightAnswer)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default,   handler: askQuestion))
+            present(ac, animated: true)
+        }
+       
 
+        trackQuestionAndEndGame()
+	}
+
+}
